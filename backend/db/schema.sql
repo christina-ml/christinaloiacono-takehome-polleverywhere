e8096e888587b39b8ae7f2dd4ce563bcf65552d1
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-18 22:55:17 -0500

    Add raffle_id to participants in schema & seed files

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -18,7 +18,8 @@ CREATE TABLE participants (
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    phone VARCHAR(255)
+    phone VARCHAR(255),
+    raffle_id INTEGER REFERENCES raffles(id) ON DELETE CASCADE
 );
 
 CREATE TABLE winners (
--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -6,10 +6,10 @@ INSERT INTO raffles (name, secret_token) VALUES
 ('Sports Raffle', '2H4s3w'),
 ('PollEv Beach Getaway Raffle', '854w9n');
 
-INSERT INTO participants (firstname, lastname, email, phone) VALUES
-('Christina', 'Loiacono', 'christinaloiacono@pursuit.org', '555-123-4567'),
-('Tim', 'Asprec', 'tim@pursuit.org', '555-321-8765'),
-('Pak', 'Chu', 'pak@pursuit.org', null);
+INSERT INTO participants (firstname, lastname, email, phone, raffle_id) VALUES
+('Christina', 'Loiacono', 'christinaloiacono@pursuit.org', '555-123-4567', 2),
+('Tim', 'Asprec', 'tim@pursuit.org', '555-321-8765', null),
+('Pak', 'Chu', 'pak@pursuit.org', null, 1);
 
 INSERT INTO winners(raffle_winner_id, participant_winner_id) VALUES
 (1, 3),
