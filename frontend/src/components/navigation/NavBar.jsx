98bb0eb52ae2360afcf31134ab4ab42538a229fb
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-20 23:50:35 -0500

    Add link to app logo, fix scss styling

--- frontend/src/components/navigation/NavBar.jsx
+++ frontend/src/components/navigation/NavBar.jsx
@@ -1,21 +1,24 @@
-import './NavBar.scss';
+import { Link } from "react-router-dom";
+import "./NavBar.scss";
 
 const NavBar = () => {
-  return (
-    <div className="NavBar">
-      <div className="NavBar__container">
-        <div className="NavBar__container__logo">
-            <img 
-                src="https://22544793.fs1.hubspotusercontent-na1.net/hubfs/22544793/PE%20Logo-1.png"
-                alt="logo"
-                height="30px"
-                width="30px"
-            />
-            <div>Raffle App</div>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="NavBar">
+			<div className="NavBar__container">
+                <Link to="/" >
+                    <div className="NavBar__container__logo">
+						<img
+							src="https://22544793.fs1.hubspotusercontent-na1.net/hubfs/22544793/PE%20Logo-1.png"
+							alt="logo"
+							height="30px"
+							width="30px"
+						/>
+						<div className="NavBar__container__logo__title">Raffle App</div>
+                    </div>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
-export default NavBar
+export default NavBar;
