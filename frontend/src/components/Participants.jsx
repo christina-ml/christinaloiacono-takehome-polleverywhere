8ce868ceadc8d56ac9b176eb62d6da7167e9da2e
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-21 19:32:48 -0500

    Add conditional rendering for if there are participants for a raffle, add current raffle name

--- frontend/src/components/Participants.jsx
+++ frontend/src/components/Participants.jsx
@@ -1,5 +1,6 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import axios from "axios";
 import RaffleNav from "./navigation/RaffleNav";
 import ParticipantDetails from "./ParticipantDetails";
@@ -11,46 +12,60 @@ import { IoSearchSharp } from "react-icons/io5";
 // Material UI
 import { IconButton, InputBase, Paper } from "@mui/material";
 
-const Participants = () => {
+const Participants = ({ raffles }) => {
 	const API = process.env.REACT_APP_API_URL;
 	const [participants, setParticipants] = useState([]);
-	const [searchTerm, setSearchTerm] = useState('');
+	const [searchTerm, setSearchTerm] = useState("");
 
 	let { id } = useParams();
 
+	// Filter raffles to get name of current raffle
+	let filteredRaffles = raffles.filter((oneRaffle) => oneRaffle.id === Number(id));
+	// console.log("filteredRaffles", filteredRaffles[0].name)
+
 	useEffect(() => {
-		axios
+		try {
+			axios
 			.get(API + `/raffles/${id}/participants`)
 			.then((res) => {
 				setParticipants(res.data);
 			})
 			.catch((err) => {
-				console.log(err);
+				console.log( err);
 			});
+		} catch (err) {
+			console.log(err);
+		}
 	}, [API, id]);
 
 	let filteredParticipants = participants;
 
 	// search by participant name
-	if (searchTerm){
-		filteredParticipants = participants.filter(participant => {
-			const participantNameToLowerCase = `${participant.firstname} ${participant.lastname}`.toLowerCase();
-			return participantNameToLowerCase.includes(searchTerm.toLowerCase());
-		})
+	if (searchTerm) {
+		filteredParticipants = participants.filter((participant) => {
+			const participantNameToLowerCase =
+				`${participant.firstname} ${participant.lastname}`.toLowerCase();
+			return participantNameToLowerCase.includes(
+				searchTerm.toLowerCase()
+			);
+		});
 	}
 
 	const handleSearchTerm = (e) => {
 		setSearchTerm(e.target.value);
-	}
+	};
 
 	return (
 		<div className="Participants">
-			<div className="Participants__currentRaffleName">{}</div>
+			<div className="Participants__currentRaffleName">
+				{filteredRaffles[0].name}
+			</div>
 			<RaffleNav />
 			<div className="Participants__all">
 				<div className="Participants__all__title">
 					Participants: {participants.length} total
 				</div>
+				{(filteredParticipants.length > 0) && 
 				<Paper
 					component="form"
 					sx={{
@@ -77,9 +92,19 @@ const Participants = () => {
 						<IoSearchSharp />
 					</IconButton>
 				</Paper>
-				{filteredParticipants.map((participant) => {
-					return <ParticipantDetails participant={participant} />;
-				})}
+				}
+				{filteredParticipants.length ? 
+				filteredParticipants.map((participant) => {
+					return (
+					<ParticipantDetails 
+						participant={participant}
+						key={participant.id + uuidv4()}
+					/>
+					)
+				}
+				)
+			: <div>No participants for this raffle yet.</div>
+			}
 			</div>
 		</div>
 	);
