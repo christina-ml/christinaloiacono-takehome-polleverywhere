d177a656cb5738f144e2ca8fcbe2f76558a524ba
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-22 20:47:19 -0500

    Add conditional rendering for search term input, handle errors. Add MUI component and styling for search bar

--- frontend/src/components/Participants.jsx
+++ frontend/src/components/Participants.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import RaffleNav from "./navigation/RaffleNav";
 import ParticipantDetails from "./ParticipantDetails";
@@ -10,7 +10,7 @@ import "./Participants.scss";
 import { IoSearchSharp } from "react-icons/io5";
 
 // Material UI
-import { IconButton, InputBase, Paper } from "@mui/material";
+import { Box, IconButton, InputBase, Paper } from "@mui/material";
 
 const Participants = ({ raffles }) => {
 	const API = process.env.REACT_APP_API_URL;
@@ -20,19 +20,21 @@ const Participants = ({ raffles }) => {
 	let { id } = useParams();
 
 	// Filter raffles to get name of current raffle
-	let filteredRaffles = raffles.filter((oneRaffle) => oneRaffle.id === Number(id));
+	let filteredRaffles = raffles.filter(
+		(oneRaffle) => oneRaffle.id === Number(id)
+	);
 	// console.log("filteredRaffles", filteredRaffles[0].name)
 
 	useEffect(() => {
 		try {
 			axios
-			.get(API + `/raffles/${id}/participants`)
-			.then((res) => {
-				setParticipants(res.data);
-			})
-			.catch((err) => {
-				console.log( err);
-			});
+				.get(API + `/raffles/${id}/participants`)
+				.then((res) => {
+					setParticipants(res.data);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
 		} catch (err) {
 			console.log(err);
 		}
@@ -65,46 +67,54 @@ const Participants = ({ raffles }) => {
 				<div className="Participants__all__title">
 					Participants: {participants.length} total
 				</div>
-				{(filteredParticipants.length > 0) && 
-				<Paper
-					component="form"
-					sx={{
-						mt: 1,
-						mb: 2,
-						p: "2px 4px",
-						display: "flex",
-						alignItems: "center",
-					}}
-				>
-					<InputBase
-						sx={{ ml: 1, flex: 1 }}
-						placeholder="Search.."
-						inputProps={{ "aria-label": "search.." }}
-						value={searchTerm}
-						onChange={handleSearchTerm}
-					/>
-					<IconButton
-						type="button"
-						sx={{ p: "10px" }}
-						aria-label="search"
-						disabled
+				{(searchTerm || filteredParticipants.length > 0) && (
+					<Box
+						component="form"
+						variant="outlined"
+						sx={{
+							mt: 1,
+							mb: 2,
+							display: "flex",
+							alignItems: "center",
+							border: "1px solid #D1D1D1",
+							borderRadius: "5px",
+						}}
 					>
-						<IoSearchSharp />
-					</IconButton>
-				</Paper>
-				}
-				{filteredParticipants.length ? 
-				filteredParticipants.map((participant) => {
-					return (
-					<ParticipantDetails 
-						participant={participant}
-						key={participant.id + uuidv4()}
-					/>
-					)
-				}
-				)
-			: <div>No participants for this raffle yet.</div>
-			}
+						<InputBase
+							sx={{ ml: 1.5, flex: 1 }}
+							placeholder="Search.."
+							inputProps={{ "aria-label": "search.." }}
+							value={searchTerm}
+							onChange={handleSearchTerm}
+						/>
+						<IconButton
+							type="button"
+							sx={{ p: "10px" }}
+							aria-label="search"
+							disabled
+						>
+							<IoSearchSharp />
+						</IconButton>
+					</Box>
+				)}
+				{filteredParticipants.length ? (
+					filteredParticipants.map((participant) => {
+						return (
+							<ParticipantDetails
+								participant={participant}
+								key={participant.id + uuidv4()}
+							/>
+						);
+					})
+				) : (
+					<div>
+						{searchTerm ? (
+							<div>Participant not found for this raffle.</div>
+						) : (
+							<div>No participants for this raffle yet.</div>
+						)}
+					</div>
+				)}
 			</div>
 		</div>
 	);
