c12458c02f9052fca47227651d88eb6e60129556
Christina Loiacono <loiaconochristina@gmail.com>
2024-01-23 23:29:30 -0500

    Add Loading component and SCSS styling

---
+++ frontend/src/helpers/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.scss";
+
+function Loading() {
+	return (
+		<div class="Loading">
+            <div class="Loading__spinner"></div>
+        </div>
+	);
+}
+
+export default Loading;
